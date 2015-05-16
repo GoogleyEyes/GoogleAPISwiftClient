27d4460619ccff0c452b1785bc36b6ba0abd9b61
Matthew Wyskiel <mwyskiel@optonline.net>
2015-05-16 18:25:05 -0400

    Initial Commit

---
+++ GoogleClient/GoogleClient.h
@@ -0,0 +1,19 @@
+//
+//  GoogleClient.h
+//  GoogleClient
+//
+//  Created by Matthew Wyskiel on 5/16/15.
+//  Copyright (c) 2015 Matthew Wyskiel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GoogleClient.
+FOUNDATION_EXPORT double GoogleClientVersionNumber;
+
+//! Project version string for GoogleClient.
+FOUNDATION_EXPORT const unsigned char GoogleClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleClient/PublicHeader.h>
+
+
