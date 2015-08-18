d4799119ae61784f1521afdf2091273b6ee0f62a
Matthew Wyskiel <mwyskiel@optonline.net>
2015-08-17 23:42:56 -0400

    update pods; make sure everything works

--- Podfile
+++ Podfile
@@ -2,11 +2,9 @@ platform :ios, '8.0'
 use_frameworks!
 
 target 'GoogleClient' do
-  pod 'GoogleSignInSDK', '~> 1.0.0'
-  pod 'Alamofire', '~> 1.2'
-  pod 'PromiseKit', '~> 2.0'
+  pod 'Alamofire', '~> 1.3'
+  pod 'PromiseKit', '~> 2.2'
   pod 'ObjectMapper'
-  pod 'AlamofireObjectMapper'
 end
 
 target 'GoogleClientTests' do
--- Pods/Target Support Files/Pods-GoogleClient/Pods-GoogleClient-frameworks.sh
+++ Pods/Target Support Files/Pods-GoogleClient/Pods-GoogleClient-frameworks.sh
@@ -53,14 +53,12 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework 'Pods-GoogleClient/Alamofire.framework'
-  install_framework 'Pods-GoogleClient/AlamofireObjectMapper.framework'
   install_framework 'Pods-GoogleClient/OMGHTTPURLRQ.framework'
   install_framework 'Pods-GoogleClient/ObjectMapper.framework'
   install_framework 'Pods-GoogleClient/PromiseKit.framework'
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework 'Pods-GoogleClient/Alamofire.framework'
-  install_framework 'Pods-GoogleClient/AlamofireObjectMapper.framework'
   install_framework 'Pods-GoogleClient/OMGHTTPURLRQ.framework'
   install_framework 'Pods-GoogleClient/ObjectMapper.framework'
   install_framework 'Pods-GoogleClient/PromiseKit.framework'
